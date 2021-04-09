8b3f6d85ac11bfe8de48f8ab88766701a142863f
Joe Sharp <ratracejoe@hotmail.com>
2021-04-09 23:28:05 +0100

    multiplication done

--- projects/04/mult/Mult.asm
+++ projects/04/mult/Mult.asm
@@ -10,3 +10,34 @@
 // R0 >= 0, R1 >= 0, and R0*R1 < 32768.
 
 // Put your code here.
+// numberOfTimes = R[0]
+// R[1] = base number
+@total // 16
+M=0
+
+@R0
+D=M+1
+@n // 17
+M=D
+
+(LOOP)
+@n
+M=M-1
+D=M
+@END
+D;JEQ // if n === 0, GOTO END
+
+@total
+D=M
+@R1
+D=D+M
+@total
+M=D
+
+@LOOP
+0;JEQ
+
+
+(END)
+@END
+0;JMP
